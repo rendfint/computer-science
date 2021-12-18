e98e93d71db5746a53eb9048b185475a9d3ccc63
rendfint <rendfint@gmail.com>
2021-12-18 01:00:38 -0500

    Assignment 0 completed

---
+++ Courses/intro_to_cs/assignment_0/ps0.py
@@ -0,0 +1,10 @@
+import math
+
+x = input("Enter X: ")
+y = input("Enter Y: ")
+
+x_int = int(x)
+y_int = int(y)
+
+print("X to the power of Y:\n", x_int**y_int)
+print("Log base two of X:\n", math.log(x_int, 2))
